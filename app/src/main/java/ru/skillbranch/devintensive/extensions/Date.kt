57f9fb26ba9991c8ff3c47435dadaee49ca63bf0
vadfe <vadafe@gmail.com>
2019-07-04 21:14:43 +0300

    hometask_2  add tasks fix2

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -66,6 +66,30 @@ enum class TimeUnits{
     SECOND,
     MINUTE,
     HOUR,
-    DAY
+    DAY;
+    fun plural(value:Int): String? {
+        val mapa  = mapOf(SECOND to listOf("секунду","секунды","секунд"),
+            MINUTE to listOf("минуту","минуты","минут"),
+            HOUR to listOf("час","часа","часов"),
+            DAY to listOf("день","дня","дней"))
+        val li = mapa.get(this)
+        val z = value % 10
+        var pos:Int? = when(value){
+            in 10..20 -> 2
+            in 110..120 -> 2
+            in 210..220 -> 2
+            in 310..320 -> 2
+            else  -> null
+        }
+        if(pos == null)
+            pos= when(z){
+                0 -> 2
+                in 2..4 -> 1
+                in 5..9 -> 2
+                1 -> 0
+                else  -> 0
+            }
+        return "$value ${li?.getOrNull(pos)}"
+    }
 }
 
