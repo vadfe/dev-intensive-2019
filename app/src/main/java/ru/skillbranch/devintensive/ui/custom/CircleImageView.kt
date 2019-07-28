8a9dc92da85c89ee0eccab1b154effae7b83df1f
vadfe <vadafe@gmail.com>
2019-07-28 21:14:27 +0300

    hometask_4 first attempt restart

--- app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
@@ -6,28 +6,13 @@ import android.widget.ImageView
 import androidx.annotation.ColorRes
 import ru.skillbranch.devintensive.R
 import android.util.Log
-
-import android.graphics.Shader.TileMode
-
 import android.graphics.*
-import android.R.color
-import android.graphics.RectF
-//import java.awt.AlphaComposite.SRC_IN
-import android.graphics.PorterDuffXfermode
-import android.graphics.Paint.ANTI_ALIAS_FLAG
-import android.R.id.mask
-import android.opengl.ETC1.getHeight
-import android.opengl.ETC1.getWidth
 import android.graphics.Bitmap
-import android.graphics.PorterDuff
-import android.graphics.Paint.ANTI_ALIAS_FLAG
-import android.R.id.mask
-import android.graphics.BitmapFactory
-import android.opengl.ETC1.getHeight
-import android.opengl.ETC1.getWidth
-
-
-
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
+import android.graphics.BitmapShader
+import android.graphics.RectF
 
 class CircleImageView @JvmOverloads constructor(
     context: Context,
@@ -43,6 +28,13 @@ class CircleImageView @JvmOverloads constructor(
     //private var aspectRatio = DEFAULT_ASPECT_RATIO
     private var borderColor  = DEFAULT_BORDER_COLOR
     private var borderWidth  = DEFAULT_BORDER_WIDTH
+    private var mBitmap:Bitmap? = null
+    private  var mShaderMatrix:Matrix = Matrix()
+    private  var mBitmapWidth:Float = 0.0f
+    private  var mBitmapHeight:Float = 0.0f
+    private lateinit var mDrawableRect :RectF
+    private lateinit var mBitmapShader:BitmapShader
+    private var mBitmapPaint:Paint = Paint()
 
     init {
         if(attrs != null){
@@ -51,6 +43,7 @@ class CircleImageView @JvmOverloads constructor(
             borderWidth = a.getDimension(R.styleable.CircleImageView_cv_borderWidth, DEFAULT_BORDER_WIDTH)
             a.recycle()
         }
+
     }
 
     fun getBorderWidth():Int = borderWidth.toInt()
@@ -66,30 +59,90 @@ class CircleImageView @JvmOverloads constructor(
 
     }
 
+    private fun setup(){
+        mDrawableRect = calculateBounds()
+        mBitmapShader = BitmapShader(mBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+        mBitmapPaint = Paint()
+        mBitmapPaint.setShader(mBitmapShader)
+        mBitmapHeight = mBitmap?.height?.toFloat() ?: 0F
+        mBitmapWidth = mBitmap?.width?.toFloat() ?: 0F
+        updateShaderMatrix()
+    }
+
     override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
+        //super.onDraw(canvas)
+        initializeBitmap()
         val halfWidth = this.width / 2
         val halfHeight = this.height / 2
         val radius = Math.max(halfWidth, halfHeight)
-        val path = Path()
-        path.addCircle(halfWidth.toFloat(), halfHeight.toFloat(), radius.toFloat(), Path.Direction.CCW)
         val rect = Rect(0, 0, this.width, this.height)
-        val rectF = RectF(rect)
+        canvas?.drawCircle(rect.centerX().toFloat(), rect.centerY().toFloat(), radius.toFloat(), mBitmapPaint)
         val  paint =  Paint(Paint.ANTI_ALIAS_FLAG)
-        paint.color = Color.WHITE
-        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_IN)
-        canvas?.drawPath(path, paint)
-        paint.xfermode = null
         paint.style = Paint.Style.STROKE
         paint.strokeWidth = borderWidth
         paint.color = borderColor
-        canvas!!.drawCircle(halfWidth.toFloat(),halfHeight.toFloat(),radius.toFloat()-borderWidth/2,paint)
-
+        canvas?.drawCircle(halfWidth.toFloat(),halfHeight.toFloat(),radius.toFloat()-borderWidth/2,paint)
 
     }
 
+    private fun updateShaderMatrix(){
+        mShaderMatrix.set(null)
+        var scale = 0.5F
+        if (mBitmapWidth * mDrawableRect.height() > mDrawableRect.width() * mBitmapHeight) {
+             scale = mDrawableRect.height() / mBitmapHeight
+        } else {
+             scale = mDrawableRect.width() / mBitmapWidth
+        }
+
+        mShaderMatrix.setScale(scale, scale)
+        mBitmapShader.setLocalMatrix(mShaderMatrix)
+    }
+
+    private fun initializeBitmap() {
+        mBitmap = getBitmapFromDrawable(getDrawable())
+        setup()
+    }
+
+    private fun getBitmapFromDrawable(drawable: Drawable?): Bitmap? {
+
+        if (drawable is BitmapDrawable) {
+            return drawable.bitmap
+        }
+        try {
+            val bitmap: Bitmap
+
+            if (drawable is ColorDrawable) {
+                bitmap = Bitmap.createBitmap(2, 2, Bitmap.Config.ARGB_8888)
+            } else {
+                bitmap = Bitmap.createBitmap(drawable!!.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+            }
+
+            val canvas = Canvas(bitmap)
+            drawable?.setBounds(0, 0, canvas.width, canvas.height)
+            drawable?.draw(canvas)
+            return bitmap
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
+
+    }
+
+    private fun calculateBounds(): RectF {
+        val availableWidth = this.width - this.paddingLeft - this.paddingRight
+        val availableHeight = this.height - this.paddingTop - this.paddingBottom
+
+        val sideLength = Math.min(availableWidth, availableHeight)
+
+        val left = this.paddingLeft + (availableWidth - sideLength) / 2f
+        val top = this.paddingTop + (availableHeight - sideLength) / 2f
+
+        return RectF(left, top, left + sideLength, top + sideLength)
+    }
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         Log.d("M_CircleImageView", "onMeasure ")
+
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
     }
 }
