48f96ddf8c540db7771f51c668861545dd66fd5d
vadafe <vadafe@gmail.com>
2019-09-03 15:56:46 +0300

    hometask_5 начало ночной темы

--- app/src/main/java/ru/skillbranch/devintensive/App.kt
+++ app/src/main/java/ru/skillbranch/devintensive/App.kt
@@ -20,7 +20,7 @@ class App:Application() {
     override fun onCreate() {
         super.onCreate()
         PreferencesRepository.getAppTheme().also {
-           // AppCompatDelegate.setDefaultNightMode(it)
+            AppCompatDelegate.setDefaultNightMode(it)
         }
     }
 }
